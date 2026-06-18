a7b49414a4fe753e71b440f7827066c3ed84245f
valemoralees <valemoyam.m@gmail.com>
2026-06-18 11:23:50 -0600

    servicios nuevos agregados en JavaScript

--- js/servicios.js
+++ js/servicios.js
@@ -17,6 +17,14 @@ const servicios = [
     },
     {
         "id": 3,
+        "nombre": "Esmaltado Acrilico",
+        "descripcion": "Disfrutado de un acabado elegante y duradero, eligiendo el largo y forma deseada.",
+        "precio": 14000,
+        "categoria": "Esmaltado",
+        "imagen": "images/Acrilico.jpg"
+    },
+    {
+        "id": 4,
         "nombre": "Manicure Normal",
         "descripcion": "Deja tus uñas y manos en perfecto estado para un acabado más elegante. Incluye esmaltado regular.",
         "precio": 12500,
@@ -24,7 +32,7 @@ const servicios = [
         "imagen": "images/manicure normal.jpg"
     },
     {
-        "id": 4,
+        "id": 5,
         "nombre": "Manicure SPA",
         "descripcion": "Relájate con un masaje y una exfoliación, dejando tus uñas en perfecto estado. Incluye esmaltado semipermanente.",
         "precio": 13500,
@@ -32,7 +40,7 @@ const servicios = [
         "imagen": "images/Manicure SPA.jpg"
     },
     {
-        "id": 5,
+        "id": 6,
         "nombre": "Pedicure Normal",
         "descripcion": "Deja tus pies arreglados con un acabado distinto.",
         "precio": 12500,
@@ -40,14 +48,39 @@ const servicios = [
         "imagen": "images/Pedi normal.jpg"
     },
     {
-        "id": 6,
+        "id": 7,
         "nombre": "Pedicure SPA",
         "descripcion": "Relájate con un masaje y tina caliente para que tus pies queden con un acabado perfecto. Incluye esmaltado semi permanente.",
         "precio": 13500,
         "categoria": "Pedicure",
         "imagen": "images/pedi spa.jpg"
+    },
+    {
+        "id": 8,
+        "nombre": "Esmaltado con Calcio",
+        "descripcion": "Una capa extra de fortaleza con calcio para la salud de tus uñas.",
+        "precio": 13500,
+        "categoria": "Adicionales",
+        "imagen": "images/calcio.jpg"
+    }
+    {
+        "id": 9,
+        "nombre": "Corte de Uñas",
+        "descripcion": "Dejamos tus uñas en la forma y largo deseado.",
+        "precio": 500,
+        "categoria": "Adicionales",
+        "imagen": "images/corte uñas.jpg"
+    },
+    {
+        "id": 10,
+        "nombre": "Remover producto",
+        "descripcion": "Un adicional por remover procedimientos realizados en otros salones distintos a Rose Glow.",
+        "precio": 700,
+        "categoria": "Adicionales",
+        "imagen": "images/quitar producto.jpg"
     }
 ];
+
 
 const serviciosGrid =
     document.getElementById("serviciosGrid");
